0c40bb7c7638546cd6ce224d816abeaf8377ba6b
Armeuu <ahmadrizqimaulanaa@gmail.com>
2025-10-19 18:47:48 +0700

    Initial commit: Halo Git

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+System.out.printf("Halo Git!");
+
+        for (int i = 1; i <= 5; i++) { // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
