f4760b90dc511b6964c5dc5b662fb66c30f5eeae
Armeuu <ahmadrizqimaulanaa@gmail.com>
2025-10-19 18:52:31 +0700

    Update: Di ubah dlu yach

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-System.out.printf("Halo Git!");
+System.out.printf("Halo Git Kedua!");
 
         for (int i = 1; i <= 5; i++) { // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
