fa2420f0e8577cfce4ad623e3f3fb4a72e494bd9
Armeuu <ahmadrizqimaulanaa@gmail.com>
2025-10-20 08:03:34 +0700

    New Commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-System.out.printf("Halo Git Kedua!");
+System.out.printf("Halo Git Ketiga!");
 
-        for (int i = 1; i <= 5; i++) { // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
     }
