882f6beaa803d05d9415640796b42d65c9bf8d65
Armeuu <ahmadrizqimaulanaa@gmail.com>
2025-10-20 08:15:02 +0700

    New Commit #2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 System.out.printf("Halo Git Ketiga!");
+        System.out.println("Ini adalah revolusi");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
